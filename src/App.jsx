cf22830f1f2bc4bcd4bf4e166283df97d2af10cb
VPVelthuizen <vpvelthuizen@gmail.com>
2024-12-26 14:47:29 -0500

    Added in starter bits for router, need to return

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Landing from './pages/landing'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; 
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Landing />
+      </div>
+    </BrowserRouter>
   );
 }
 
