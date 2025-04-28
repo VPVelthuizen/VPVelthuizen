d340eaf25ee5ec56b84a90e097ca795879be127e
VPVelthuizen <vpvelthuizen@gmail.com>
2025-04-28 08:41:32 -0400

    added router to change pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import Landing from './pages/landing'
 import About from './pages/about'
+import Projects from './pages/projects'
+import Resume from './pages/resume'
+import Contact from './pages/contact'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -10,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/about" element={<About />} />
-
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
